f0df4cd9349068be343837b7ebdaa8bb7289a282
jereq1 <jereq1@student.monash.edu>
2021-07-04 11:51:20 +1000

    enabled textures from the texture chapter of learn openGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,31 @@
-#define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "shader.h"
-
-#include <sstream>
-#include <fstream>
 #include <iostream>
 #include <cmath>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include "shader.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
-static void GLClearError();
-static void GLCheckError();
-
-int main(void)
+int main()
 {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// Forward compat for macOS
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
 
-	GLuint WINDOW_WIDTH = 800;
-	GLuint WINDOW_HEIGHT = 600;
+	// Forward Compatibility with Mac OS X
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	// Initialize the window
-	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "LearnOpenGL", NULL, NULL);
+	// std::cout << "Hello World!! FUCK" << std::endl;
+
+
+	// Create a window!
+	GLFWwindow* window = glfwCreateWindow(1200, 900, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
@@ -36,175 +33,163 @@ int main(void)
 		return -1;
 	}
 
-	// Make the current window the context
+	// This function makes the OpenGL or OpenGL ES context of the specified window current on the calling thread. 
+	// A context must only be made current on a single thread at a time and each thread can have only a single 
+	// current context at a time.
 	glfwMakeContextCurrent(window);
 
-	// Initialie glad after makng a window
+	// Make Context current first before initialising glad
+	// https://stackoverflow.com/questions/48650497/glad-failing-to-initialize
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	// Print GL Version
-	std::cout << glGetString(GL_VERSION) << std::endl;
+	// Render the window
+	glViewport(0, 0, 1200, 900);
 
-	// issues on retina display such that the width and height of the viewport
-	// must be the same as the frame buffer size
-	// https://stackoverflow.com/questions/31303291/opengl-rendering-constricted-to-bottom-left-quarter-of-the-screen
-	GLint frameBufferWidth, frameBufferHeight;
-	glfwGetFramebufferSize(window, &frameBufferWidth, &frameBufferHeight);
-	// Set the size of the window view port
-	glViewport(0, 0, frameBufferWidth, frameBufferHeight);
-
-	// Resize the viewport when the user changes the size
+	// tell GLFW we want to call this function on every window resize by registering it
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	// int nrAttributes;
+	// glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+	// std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
 
-	// Vertices to draw an equilateral triangle
-	GLfloat vertices[] = 
-	{	// positions        // Colours
-		-0.5f,  -0.5f,  1.0f,  0.0f,  1.0f,  0.0f,	 // 0 - bottom left
-		 0.5f,  -0.5f,  0.0f,  0.0f,  0.0f,  1.0f,   // 1 - bottom right
-		 0.0f,   0.5f,  0.0f,  1.0f,  0.0f,  0.0f	 // 2 - top middle
+	const char *vertexShaderSource = "shaderSource/VertShader.glsl";
+	const char *fragmentShaderSource = "shaderSource/FragShader.glsl";
+	// Use Create and Compile, and Create a Shader Program
+	Shader ourShader(vertexShaderSource, fragmentShaderSource);
+
+	// vertices of our triangle in NDC
+	float vertices[] = 
+	{	
+		// positions        // colours        // texture coords
+		0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // top right
+		0.5f, -0.5f,  0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+	   -0.5f, -0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom left
+	   -0.5f,  0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f   // top left
 	};
 
-	unsigned int indices[] =
-	{
-		0, 1, 2
+	// indices of vertex to draw a triangle
+	unsigned int indices[] = {
+		0, 1, 2, // first triangle
+		2, 3, 0  // second triangle
 	};
 
-	// Vertices and indices to draw 3 stacking triangles on top of each other
-	GLfloat three_triangle_vert[] =
-	{
-		-0.5f, -0.5f, 0.0f, 	// 0 - bottom left
-		 0.0f, -0.5f, 0.0f,		// 1 - bottom bottom
-		 0.5f, -0.5f, 0.0f,		// 2 - bottom right
-		-0.25f, 0.0f, 0.0f,		// 3 - middle left
-		 0.25f, 0.0f, 0.0f,		// 4 - middle right
-		 0.0f,	0.5f, 0.0f		// 5 - top
-	};
+	// Vertex Buffer Objects (VBO)
+	// Vertex Array Objects (VAO)
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);	// Generate VAO object
+	glBindVertexArray(VAO);	// Bind Vertex Array Object
+	glGenBuffers(1, &VBO);	// Generate VBO object
+	glGenBuffers(1, &EBO); 	// Generate EBO object
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the VBO
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);	// Bind the EBO to an Element Array Buffer
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // Copies the defined vertex data into the buffer's memory
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); // Copies the defined vertex data into the buffer's memory
 
-	unsigned int three_triangle_indices[] =
-	{
-		0, 1, 3,
-		1, 2, 4,
-		3, 4, 5
-	};
-
-	// Create a VBO, VAO, EBO
-	// VAO = Vertex Attribute Object
-	// VBO = Vertex Buffer Object
-	// EBO = Element Buffer Object
-	GLuint VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	// Generate Buffers for VBO and EBO
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	// 1. bind Vertex Array Object
-	glBindVertexArray(VAO);
-
-	// 2. copy our vertices array in a buffer for OpenGL to use
-	//  Creates a data store for the buffer objects
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);			// Binding VBO
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	// copy index array in an element buffer for OpenGL
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO); // Binding EBO
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-	// 3. then set our vertex attributes pointers
-
+	// Tell how OpenGL should interpret the vertex data (per vertex attribute)
 	// Position Attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-	(void*)0);
-	glEnableVertexAttribArray(0); // Enable position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 
 	// Colour Attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);	// Enabled colour attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	// Create a Shader Program from a vertex shader source and fragment shader source
-	const char *vertexShaderSource = "shaders/VertShader.glsl";
-	const char *fragmentShaderSource = "shaders/FragShader.glsl";
+	// Texture attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-	// Shader Program Constur
-	Shader ourShader(vertexShaderSource, fragmentShaderSource);
-	
-	// Wireframe mode if enabled
+	/*Usually when you haave multiple objects you want to draw, you first generate/configure all the VAOs
+	(and thus the required VBO and attribute pointers) and store those for later use. The momenet we want
+	to draw one of our objects, we take the corresponding VAO, bind it, then draw the object and unbind the
+	VAO again
+	*/ 
+
+	// whole process of generating a texture 
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// set the texture wrapping/filtering options (on currently bound texture)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load and generate the texture
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("textures/container.jpg", &width, &height,
+	&nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+		GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+
+	stbi_image_free(data);
+
+	// Wireframe mode
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	// enable blending
-	glEnable(GL_BLEND);
-	// Blend function
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	// Render Loop
 	while(!glfwWindowShouldClose(window))
-	{
-		// Handle input
+	{		
+		// Process user keyboard input
 		processInput(window);
 
-		// ... Render here
-		// Clear the colourbufer
+		// Rendering commands here
+		// Clear the colorbuffers
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Use the Shader Program
-		// glUseProgram(shaderProgram);
+		// Use the shader program
 		ourShader.use();
 
-		// update the alpha value and set the triangle to change alpha values
-		float timeValue = glfwGetTime();
-		float alphaValue = std::sin(timeValue) / 2.0f + 0.5f;
-		ourShader.setFloat("ourAlpha", alphaValue);
+		// Determine the colour of the triangle
+		// float timeValue = glfwGetTime();
+		// float greenValue = (std::sin(timeValue) / 2.0f) + 0.5f;
+		// printf("Green colour value : %1.1f\n", greenValue);
+		// // std::cout << "Green Colour Value: %1.1f" <<
+		// int vertexColourLocation = glGetUniformLocation(shaderProgram, "ourColour");
+		// glUniform4f(vertexColourLocation, 0.0f, greenValue, 0.0f, 1.0f);	// change the green colour value
 
-		// Drawing code in render loop
-		// glDrawArrays(GL_TRIANGLES, 0, 6);
+		// Render the an orange triangle
+		glBindTexture(GL_TEXTURE_2D, texture);
 		glBindVertexArray(VAO);
-		GLClearError();
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-		GLCheckError();
-
-		// Unbind the VAO
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
 
-		// check and call events and swap the buffers
+		// Check and call events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	// terminiate glfw
+	// Terminate GLFW
 	glfwTerminate();
+
 	return 0;
 }
 
-static void GLClearError()
-{
-	while (glGetError() != GL_NO_ERROR)
-	{
-		
-	}
-}
-
-static void GLCheckError()
-{
-	while (GLenum error = glGetError())
-	{
-		std::cout << "[OpenGL Error] (0x" << std::hex << error << ")"  << std::endl;
-	}
-}
-
-// Resizes the viewport
+// Resize the viewport when user resizes the window
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
 }
 
-// Process Keyboard input
+// Process keyboard input
 void processInput(GLFWwindow *window)
 {
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-	glfwSetWindowShouldClose(window, true);
+	{
+		glfwSetWindowShouldClose(window, true);	// close the window when user presses escape
+	}
 }
