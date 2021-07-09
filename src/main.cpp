c167c4b1d56457a0a6728e6d38a6fe96acf68adb
jereq1 <jereq1@student.monash.edu>
2021-07-10 00:31:03 +1000

    completed rotation of the container using glm

--- src/main.cpp
+++ src/main.cpp
@@ -43,14 +43,6 @@ int main()
 	// current context at a time.
 	glfwMakeContextCurrent(window);
 
-	// See if we can put transformation by translating a vector of (1,0,0) by (1,1,0)
-	// Note that we define it as glm::vec4 with its homogenous coordinate set 1.0:
-	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
-	glm::mat4 trans = glm::mat4(1.0f);
-	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
-	vec = trans * vec;
-	std::cout << vec.x << vec.y << vec.z << std::endl;
-
 	// Make Context current first before initialising glad
 	// https://stackoverflow.com/questions/48650497/glad-failing-to-initialize
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -194,6 +186,13 @@ int main()
 	ourShader.setInt("texture1", 0);
 	ourShader.setInt("texture2", 1);
 
+	// Note that we define it as glm::vec4 with its homogenous coordinate set 1.0:
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+
+	unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
 	// Render Loop
 	while(!glfwWindowShouldClose(window))
